14422916cac38538ccdcb1d10ae3f7592fd40054
majabukowska <majabukowska8@gmail.com>
2023-11-21 13:25:40 +0100

    added favicon and base

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+import os
+
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
